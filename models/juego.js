65f69e3802ff7b19018be343152952d0e6aafb67
Fa1323 <112007478+Fa1323@users.noreply.github.com>
2024-08-28 01:32:42 -0600

    Lista de espera y de espectadores

--- models/juego.js
+++ models/juego.js
@@ -12,7 +12,7 @@ const WINNER_COMBOS = [
 class Juegos {
     constructor(io) {
         this.activeGames = {};
-        this.io = io; 
+        this.io = io;
     }
 
     createGame(roomId, jugador1, jugador2) {
@@ -25,11 +25,11 @@ class Juegos {
 
     makeMove(roomId, index, jugadorId) {
         const juego = this.activeGames[roomId];
-        
+
         if (juego && juego.board[index] === null && juego.turn === (jugadorId === juego.players[0] ? 'X' : 'O')) {
             juego.board[index] = juego.turn;
             juego.turn = juego.turn === 'X' ? 'O' : 'X';
-            
+
             return {
                 board: juego.board,
                 turn: juego.turn,
@@ -68,11 +68,11 @@ class Juegos {
     iniciarJuego(ListaDeEspera, JugadoresEnJuego) {
         if (ListaDeEspera.length >= 2) {
             const jugador1 = ListaDeEspera.shift();
-            const jugador2 = ListaDeEspera.shift(); 
+            const jugador2 = ListaDeEspera.shift();
 
-            const roomId = `${jugador1.id}+${jugador2.id}`; 
-            jugador1.join(roomId); 
-            jugador2.join(roomId); 
+            const roomId = `${jugador1.id}+${jugador2.id}`;
+            jugador1.join(roomId);
+            jugador2.join(roomId);
 
             JugadoresEnJuego.push({ id: jugador1.id, roomId });
             JugadoresEnJuego.push({ id: jugador2.id, roomId });
@@ -86,61 +86,83 @@ class Juegos {
 
     removerJugadorPerdedor(perdedorId, roomId, ListaDeEspera, JugadoresEnJuego) {
         JugadoresEnJuego = JugadoresEnJuego.filter(player => player.id !== perdedorId);
-    
+
         const perdedorSocket = this.io.sockets.sockets.get(perdedorId);
         if (perdedorSocket) {
             ListaDeEspera.push(perdedorSocket);
             console.log(`Jugador ${perdedorId} ha sido añadido a la lista el perdedor`);
         }
-    
+
         if (ListaDeEspera.length > 0) {
             const nuevoJugador = ListaDeEspera.shift();
             nuevoJugador.join(roomId);
-    
+
             JugadoresEnJuego.push({ id: nuevoJugador.id, roomId });
-    
+
         } else {
             this.endGame(roomId);
             console.log(`No hay más jugadores en la lista de espera. `);
         }
     }
-    
-    removerJugadoresEmpate(roomId, ListaDeEspera, JugadoresEnJuego) {
-        const juego = this.activeGames[roomId];
-    
-        if (juego) {
-            const [jugador1Id, jugador2Id] = juego.players;
-    
-            JugadoresEnJuego = JugadoresEnJuego.filter(player => player.id !== jugador1Id && player.id !== jugador2Id);
-    
-            const jugador1Socket = this.io.sockets.sockets.get(jugador1Id);
-            const jugador2Socket = this.io.sockets.sockets.get(jugador2Id);
-    
-            // Añadir ambos jugadores a la lista de espera
-            if (jugador1Socket) {
-                ListaDeEspera.push(jugador1Socket);
-                console.log(`Jugador ${jugador1Id} añadido a la lista de espera `);
-            }
-            if (jugador2Socket) {
-                ListaDeEspera.push(jugador2Socket);
-                console.log(`Jugador ${jugador2Id} añadido a la lista de espera`);
-            }
 
-    //  Para poder mandar cosas es en menssage
-            if (jugador1Socket) jugador1Socket.emit('gameEnd', { message: 'Empate. se va a lista de espera' });
-            if (jugador2Socket) jugador2Socket.emit('gameEnd', { message: 'Empate. se va a lista de espera' });
-    
-            this.endGame(roomId);
-    
-            // Reiniciar el juego  en la lista de espera
-            if (ListaDeEspera.length >= 2) {
-                this.iniciarJuego(ListaDeEspera, JugadoresEnJuego);
-                console.log(`Se ha reiniciado el juego con los jugadores disponibles en la lista de espera.`);
+    removerJugadoresEmpate(roomId, ListaDeEspera, JugadoresEnJuego) {
+            const juego = this.activeGames[roomId];
+
+            if (juego) {
+                const [jugador1Id, jugador2Id] = juego.players;
+
+                JugadoresEnJuego = JugadoresEnJuego.filter(player => player.id !== jugador1Id && player.id !== jugador2Id);
+
+                const jugador1Socket = this.io.sockets.sockets.get(jugador1Id);
+                const jugador2Socket = this.io.sockets.sockets.get(jugador2Id);
+
+                // Añadir ambos jugadores a la lista de espera
+                if (jugador1Socket) {
+                    ListaDeEspera.push(jugador1Socket);
+                    console.log(`Jugador ${jugador1Id} añadido a la lista de espera `);
+                }
+                if (jugador2Socket) {
+                    ListaDeEspera.push(jugador2Socket);
+                    console.log(`Jugador ${jugador2Id} añadido a la lista de espera`);
+                }
+
+                //  Para poder mandar cosas es en menssage
+                if (jugador1Socket) jugador1Socket.emit('gameEnd', { message: 'Empate. se va a lista de espera' });
+                if (jugador2Socket) jugador2Socket.emit('gameEnd', { message: 'Empate. se va a lista de espera' });
+
+                this.endGame(roomId);
+
+                // Reiniciar el juego  en la lista de espera
+                if (ListaDeEspera.length >= 2) {
+                    this.iniciarJuego(ListaDeEspera, JugadoresEnJuego);
+                    console.log(`Se ha reiniciado el juego con los jugadores disponibles en la lista de espera.`);
+                }
             }
         }
+        //funcion para irse a la lista de espectadores
+    moverAEspectadores(jugadorId, ListaDeEspera, ListaDeEspectadores, emitirActualizacion) {
+        const index = ListaDeEspera.findIndex(jugador => jugador.id === jugadorId);
+        if (index !== -1) {
+            const [jugador] = ListaDeEspera.splice(index, 1);
+            ListaDeEspectadores.push(jugador);
+            if (emitirActualizacion) emitirActualizacion();
+            console.log(`Jugador ${jugadorId} movido a espectadores`);
+        }
     }
-    
-    
+
+    //Funcion para mover a la lista de espera 
+
+    moverAListaDeEspera(jugadorId, ListaDeEspectadores, ListaDeEspera, emitirActualizacion) {
+        const index = ListaDeEspectadores.findIndex(jugador => jugador.id === jugadorId);
+        if (index !== -1) {
+            const [jugador] = ListaDeEspectadores.splice(index, 1);
+            ListaDeEspera.push(jugador);
+            if (emitirActualizacion) emitirActualizacion();
+            console.log(`Jugador ${jugadorId} movido a lista de espera`);
+        }
+    }
+
+
 }
 
 module.exports = Juegos;
--- models/sockets.js
+++ models/sockets.js
@@ -3,9 +3,10 @@ const Juegos = require('./juego');
 class Sockets {
     constructor(io) {
         this.io = io;
-        this.ListaDeEspera = []; 
+        this.ListaDeEspera = [];
+        this.ListaDeEspectadores = [];
         this.JugadoresEnJuego = [];
-        this.juegos = new Juegos(io); 
+        this.juegos = new Juegos(io);
 
         this.socketEvents();
     }
@@ -16,42 +17,42 @@ class Sockets {
 
             this.ListaDeEspera.push(socket);
             console.log('Usuarios en lista de espera', this.ListaDeEspera.map(s => s.id));
-        
+
             if (this.ListaDeEspera.length >= 2) {
                 this.juegos.iniciarJuego(this.ListaDeEspera, this.JugadoresEnJuego);
             }
 
             socket.on('makeMove', (data) => {
                 const { roomId, index } = data;
-            
+
                 const moveResult = this.juegos.makeMove(roomId, index, socket.id);
-            
+
                 if (moveResult) {
                     const { board, turn, winner, isDraw } = moveResult;
-            
+
                     if (winner) {
                         this.io.to(roomId).emit('gameEnd', { winner });
                         console.log(`Juego terminado`);
-                         //se elimina cuando pierde
+                        //se elimina cuando pierde
                         this.juegos.removerJugadorPerdedor(socket.id, roomId, this.ListaDeEspera, this.JugadoresEnJuego);
-            
+
                     } else if (isDraw) {
                         this.juegos.endGame(roomId);
                         console.log(`fue empate`);
 
-            
+
                         this.juegos.removerJugadoresEmpate(roomId, this.ListaDeEspera, this.JugadoresEnJuego);
                         this.juegos.resetGame(roomId);
                         this.io.to(roomId).emit('gameEnd', { winner: 'Empate' });
 
                         this.io.to(roomId).emit('gameReset', { board: Array(9).fill(null), turn: 'X' });
-            
+
                     } else {
                         this.io.to(roomId).emit('updateBoard', { board, turn });
                     }
                 }
             });
-            
+
 
             socket.on('resetGame', () => {
                 const roomId = [...socket.rooms].find(room => room !== socket.id);
@@ -80,6 +81,41 @@ class Sockets {
                 }
             });
         });
+
+        // Mueve al jugador actual de la lista de espera a la lista de espectadores
+        socket.on('moverAEspectadores', () => {
+            this.juegos.moverAEspectadores(socket.id, this.ListaDeEspera, this.ListaDeEspectadores, () => {
+                this.emitirListaDeEspera();
+                this.emitirListaDeEspectadores();
+                this.emitirConteoDeEspectadores();
+            });
+        });
+
+        // Mueve al jugador actual de la lista de espectadores a la lista de espera
+
+        socket.on('moverAListaEspera', () => {
+            this.juegos.moverAListaDeEspera(socket.id, this.ListaDeEspectadores, this.ListaDeEspera, () => {
+                this.emitirListaDeEspera();
+                this.emitirConteoDeEspectadores();
+            });
+        });
+
+    }
+
+    // Emitir la lista de espera actualizada 
+    emitirListaDeEspera() {
+        this.io.emit('actualizarListaDeEspera', this.ListaDeEspera.map(s => ({ id: s.id })));
+    }
+
+    // Emitir la lista de espectadores actualizada
+    emitirListaDeEspectadores() {
+        this.io.emit('actualizarListaDeEspectadores', this.ListaDeEspectadores.map(s => ({ id: s.id })));
+    }
+
+    // Emitir el conteo de espectadores 
+    emitirConteoDeEspectadores() {
+        const conteoEspectadores = this.ListaDeEspectadores.length;
+        this.io.emit('actualizarConteoDeEspectadores', { conteo: conteoEspectadores });
     }
 }
 
